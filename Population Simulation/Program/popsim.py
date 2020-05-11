fb949f1e5192408b65a8a34f3ac740b9367883c7
Joshua Wright <a1721037@student.adelaide.edu.au>
2020-05-11 23:01:42 +0930

    Basic linear loop population extrapolation

--- Population Simulation/Program/popsim.py
+++ Population Simulation/Program/popsim.py
@@ -1,9 +1,39 @@
-#Sample python file
-
 #Imports
 import simpy
+import matplotlib.pyplot as plt
 
-def test():
-    print("TEST")
+#Starting Population
+P0 = 100000000
+BirthRate = 0.05
+DeathRate = 0.04
+Year = 2000
 
-env = simpy.Environment()
+#Vars
+graph_years = []
+graph_popul = []
+
+#Itteration
+class Population():
+    def __init__(self):
+        self.year = Year
+        self.pop = P0
+        print(self.year, self.pop)
+
+    def simulate(self, count):
+        for i in range(count):
+            self.simyear()
+    
+    def  simyear(self):
+        self.year = self.year + 1
+        self.pop = self.pop + self.pop*BirthRate - self.pop*DeathRate
+        graph_years.append(self.year)
+        graph_popul.append(self.pop)
+        print(self.year, int(self.pop))
+        
+
+sim = Population()
+sim.simulate( 10 )
+plt.plot(graph_years, graph_popul)
+plt.ylabel('Population')
+plt.xlabel('Year')
+plt.show()
