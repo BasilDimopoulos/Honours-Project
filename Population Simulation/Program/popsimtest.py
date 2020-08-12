7850195db2d8d3bb4d9ee63b6b44fb602dd83f2d
Josh <jjhwrocks@hotmail.com>
2020-08-12 19:43:52 +0930

    Tests auto start on program run
    nolonger requires python3 -m unittest to start

--- Population Simulation/Program/popsimtest.py
+++ Population Simulation/Program/popsimtest.py
@@ -1,7 +1,13 @@
 import unittest
-import popsim
+# import popsim
 
 # Test to see if python unit test works
 class TEMP(unittest.TestCase):
     def test1(self):
         self.assertEqual(1,1)
+
+
+
+# Start unit tests
+if __name__ == '__main__':
+    unittest.main()
