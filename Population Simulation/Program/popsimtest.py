1ba5076f01ce91fa86fac1f17aecc57dfe690801
Josh <jjhwrocks@hotmail.com>
2020-08-26 23:59:08 +0930

    Changed test labeling scheme to match test codes on wiki #27

--- Population Simulation/Program/popsimtest.py
+++ Population Simulation/Program/popsimtest.py
@@ -19,29 +19,29 @@ def runCommand(cmd):
 # Class for unit test library
 class TESTING(unittest.TestCase):
     
-    # Test 1: No Arguements
-    def test1(self):
+    # Test_PS001: No Arguements
+    def test_PS001(self):
         cmd = "python3 popsim.py"
         out = runCommand(cmd)[1]
         expected = ['usage: popsim.py [-h] -i INPUT [-p POPULATION] [-d DURATION] [-l] [-ut] [-ndt]', '                 [-s SAVE] [-nd]', 'popsim.py: error: the following arguments are required: -i/--input']
         self.assertEqual(out, expected)
 
-    # Test 2: Test to see if reponse is created for 10 years
-    def test2(self):
+    # Test_PS002: Test to see if reponse is created for 10 years
+    def test_PS002(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -d 10 -nd"
         out = runCommand(cmd)[0]
         expected = ['Reading from file: test1.csv...', '2000 19028802', '2001 19150147', '2002 19267997', '2003 19385278', '2004 19504147', '2005 19764685', '2006 20055644', '2007 20390244', '2008 20788572', '2009 21262588', '2010 21669981']
         self.assertEqual(out, expected)
 
-    # Test 3: Check if Use Truth data provides a different result to when it is not used
-    def test3(self):
+    # Test_PS003: Check if Use Truth data provides a different result to when it is not used
+    def test_PS003(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -d 10 -nd -ut"
         out = runCommand(cmd)[0]
         expected = ['Reading from file: test1.csv...', '2000 19028802', '2001 19274701', '2002 19495210', '2003 19720737', '2004 19932722', '2005 20176844', '2006 20450966', '2007 20827622', '2008 21249199', '2009 21691653', '2010 22031750']
         self.assertEqual(out, expected)
 
-    # Test 4: Check to see if image output is correct
-    def test4(self):
+    # Test_PS004: Check to see if image output is correct
+    def test_PS004(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -d 10 -nd -s test4"
         out = runCommand(cmd)[0]
         img_calc  = Image.open('test4.png')
@@ -49,22 +49,22 @@ class TESTING(unittest.TestCase):
         self.assertEqual(list(img_calc.getdata()), list(img_truth.getdata()))
         runCommand("rm test4.png")
 
-    # Test 5: Test with different starting population
-    def test5(self):
+    # Test_PS005: Test with different starting population
+    def test_PS005(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -d 10 -nd -p 100"
         out = runCommand(cmd)[0]
         expected = ['Reading from file: test1.csv...', '2000 100', '2001 121444', '2002 239295', '2003 356576', '2004 475445', '2005 735983', '2006 1026942', '2007 1361542', '2008 1759870', '2009 2233886', '2010 2641279']
         self.assertEqual(out, expected)
 
-    # Test 6: Test with different number of years
-    def test6(self):
+    # Test_PS006: Test with different number of years
+    def test_PS006(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -nd"
         out = runCommand(cmd)[0]
         expected = ['Reading from file: test1.csv...', '2000 19028802', '2001 19150147', '2002 19267997', '2003 19385278']
         self.assertEqual(out, expected)
     
-    # Test 7: Test output image if no truth data is displayed
-    def test7(self):
+    # Test_PS007: Test output image if no truth data is displayed
+    def test_PS007(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -ndt -nd -s test7"
         out = runCommand(cmd)[0]
         img_calc  = Image.open("test7.png")
@@ -72,8 +72,8 @@ class TESTING(unittest.TestCase):
         self.assertEqual(list(img_calc.getdata()), list(img_truth.getdata()))
         runCommand("rm test7.png")    
 
-    # Test 8: Test output image if labels are printed
-    def test8(self):
+    # Test_PS008: Test output image if labels are printed
+    def test_PS008(self):
         cmd = "python3 popsim.py -i test_data/test1.csv -l -nd -s test8"
         out = runCommand(cmd)[0]
         img_calc  = Image.open("test8.png")
@@ -81,16 +81,16 @@ class TESTING(unittest.TestCase):
         self.assertEqual(list(img_calc.getdata()), list(img_truth.getdata()))
         runCommand("rm test8.png")    
 
-    # Test 9: Properly displays help menu
-    def test9(self):
+    # Test_PS009: Properly displays help menu
+    def test_PS009(self):
         cmd = "python3 popsim.py --help"
         out = runCommand(cmd)[0]
         expected = ['usage: popsim.py [-h] -i INPUT [-p POPULATION] [-d DURATION] [-l] [-ut] [-ndt]', '                 [-s SAVE] [-nd]', '', 'Run a population simulation on given input data.', '', 'optional arguments:', '  -h, --help            show this help message and exit', '  -i INPUT, --input INPUT', '                        Input file location (Required Value)', '  -p POPULATION, --population POPULATION', '                        Provide an integer value for starting population', '  -d DURATION, --duration DURATION', '                        Provide an integer value for number duration to run', '                        the simulate for, in years', '  -l, --labels          Enable Labels on output graph', '  -ut, --usetruth       Use truth data years for output', '  -ndt, --nodisplayedtruth', "                        Don't display truth data on output graph", '  -s SAVE, --save SAVE  Output file (.png)', '  -nd, --nodisplay      Disables gui output']
         self.assertEqual(out, expected)
 
-    # Test 10: Display test
+    # Test_PS010: Display test
     @unittest.skip("Skipping Visual Display Test")
-    def test10(self):
+    def test_PS010(self):
         cmd = "python3 popsim.py -i test_data/test1.csv"
         out = runCommand(cmd)[0]
         expected = ['Reading from file: test1.csv...', '2000 19028802', '2001 19150147', '2002 19267997', '2003 19385278']
