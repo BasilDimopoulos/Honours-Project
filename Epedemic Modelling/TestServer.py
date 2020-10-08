205a53562a8394d3001b635e6e3a0566cd858fc6
Daniel Lukis <daniel.lukis@student.adelaide.edu.au>
2020-10-08 12:48:39 +1030

    Implemented client-server control schema - Added control message parsing to TestServer.py which is a direct websocket to the client - Added functions to EpidemicModel to handle the corresponding control messages - Added test json file with sample control messages

---
+++ Epedemic Modelling/TestServer.py
@@ -0,0 +1,48 @@
+# Direct server connection to web client. For test and demonstration purposes only
+
+import asyncio
+import websockets
+import json.decoder
+import EpidemicModel
+
+
+def getAllCells():
+    print("inside getAllCells")
+
+
+def processControlMsg(msg, data):
+    print("Control message is: " + msg + "\n--------")
+
+    switcher = {
+        'initApp': EpidemicModel.initApp,
+        'nextStep': EpidemicModel.nextStep,
+        'getAllCells': EpidemicModel.getAllCells,
+    }
+    # Get the function from the switcher dictionary
+    func = switcher.get(msg, lambda: "Invalid control message.")
+    return func(data)
+
+async def comms(websocket, path):
+    async for message in websocket:
+        indat = json.loads(message)
+        print(indat)
+
+        #Do work here -------
+        # Parse the json from client to retrieve the control message
+        ctrlMsg = indat['control']
+        response = ""
+        response = processControlMsg(ctrlMsg, indat)
+        print("--------")
+        print("Response: " + response['status'])
+        print(response)
+
+        # -------
+
+
+        # json.dumps(outdat)
+        await websocket.send(response['status'])
+
+start_server = websockets.serve(comms, "localhost", 8002)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
