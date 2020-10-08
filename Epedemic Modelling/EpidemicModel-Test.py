790f8e487e585c552f6f6ef7c30a00d1fa2ae875
Daniel Lukis <daniel.lukis@student.adelaide.edu.au>
2020-10-08 13:39:08 +1030

    Updated test for timeStep

--- Epedemic Modelling/EpidemicModel-Test.py
+++ Epedemic Modelling/EpidemicModel-Test.py
@@ -20,15 +20,15 @@ class TESTING(unittest.TestCase):
     # Test 3 - Test Setters and Getters of Time Step
     def test_EM003(self):
         c1 = EM.Cell("Adelaide")
-        app.timeStep = 2
-        self.assertEqual(app.timeStep, 2)
+        EM.app.timeStep = 2
+        self.assertEqual(EM.app.timeStep, 2)
 
     # Test 4 - Test Setters Update, Outputs and Get outputs
     def test_EM004(self):
         c1 = EM.Cell("Adelaide")
         c1.setInitCond([1,27,0,0,500])
         c1.setEquationParams([2.79*(1/2.9), 0.2, 1/2.9, 0.01, 0.14])
-        app.timeStep = 2
+        EM.app.timeStep = 2
         c1.updateOutputs()
         self.assertEqual(c1.getOutputs(), (451.71881842639687, 19.61676947322724, 20.901963282821953, 7.527802888432526, 0.23464592912168014))
 
