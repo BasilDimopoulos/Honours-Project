33ff8023c153602638b10c6e97060a5e918a5bea
Joshua Wright <jjhwrocks@hotmail.com>
2020-10-15 23:46:43 +1030

    Loading visuals from JSON file and added basic interactive elements

--- Webapp/SERP Dynamic Elements/v2/mainbox.js
+++ Webapp/SERP Dynamic Elements/v2/mainbox.js
@@ -1,74 +1,90 @@
-$(document).ready(function(){
+// Global Vars
+var cellcont = [];
+var cellCharts = [];
+
+// Draw main cells
+function mainCell(num){
     var mainchart = document.getElementById("maincanvas").getContext("2d");
+    colours = [ "#FFCD40", "#4B51D0", "#39E039", "#ce77e6", "#FF4040" ];
+    console.log()
     var mainLineChart = new Chart(mainchart, {
         type: "doughnut",
         data:{
-            labels: ["A", "B"],
+            labels: ["Susceptible", "Exposed", "Infected", "Recovered", "Deaths"],
             datasets:[{
                 label: "Test",
-                data: [ 55, 60 ],
-                backgroundColor : [
-                    "rgba(255, 299, 032, 0.6)",
-                    "rgba(54, 162, 235, 0.6)"
-                ] 
+                data: [ 
+                    cellcont[num]["susceptibles"].slice(-1)[0], 
+                    cellcont[num]["exposed"].slice(-1)[0], 
+                    cellcont[num]["infected"].slice(-1)[0], 
+                    cellcont[num]["recovered"].slice(-1)[0], 
+                    cellcont[num]["deaths"].slice(-1)[0]
+                ],
+                backgroundColor : colours
             }],
         },
         options:{
             responsive:true,
-            maintainAspectRatio: false
+            maintainAspectRatio: false,
+            title: { display: true, text: cellcont[num]["name"] }
         }
     });
+}
 
-    sideCells();
-});
+// Draw sidebar cells
+function sideCells(content){    
+    
+    // Get array of time stamps for cells
+    times = [];
+    for(var i = 0; i < content[0]["population"].length; i++) times.push(i);
 
-var cellCharts = [];
-
-function sideCells(){    
-    var cells = [ "SA", "NSW", "VIC", "WA", "QLD" ];
+    
     $("#cells-stack").empty()
     cellCharts = [];
-    colours = ["#FF4040", "#FFCD40", "#4B51D0", "#39E039", "#ce77e6" ];
-    $.each(cells, function(i, key){
-        $("#cells-stack").append("<canvas id='sidecell-" + i + "' width=400px height=200px ></canvas>");
-        if(i != (cells.length - 1)) $("#cells-stack").append("<hr />");
+    colours = [ "#FFCD40", "#4B51D0", "#39E039", "#ce77e6", "#FF4040" ];
+
+
+    $.each(content, function(i, key){
+        $("#cells-stack").append("<canvas id='sidecell-" + i + "' width=" + $("#cells-stack-table").width() + "px height=200px ></canvas>");
+        if(i != (content.length - 1)) $("#cells-stack").append("<hr />");
         var current = document.getElementById("sidecell-" + i).getContext("2d");
+        isfirst = (i == 0);
         var newchart = new Chart(current, {
             type: "line",
             data:{
-                labels:[1,2,3,4,5,6,7],
+                labels: times,
                 datasets:[
                     {
                         label: "S",
-                        data: [65, 59, 80, 81, 56, 55, 40],
+                        data: key["susceptibles"],
                         borderColor: colours[0],
                         pointHighlightFill: colours[0],
                         fill: false
                     },
                     {
                         label: "E",
-                        data: [28, 48, 40, 19, 86, 27, 90],
+                        data: key["exposed"],
                         borderColor: colours[1],
                         pointHighlightFill: colours[1],
                         fill: false
                     },
                     {
                         label: "I",
-                        data: [1,22,13,26,8,7,42],
+                        data: key["infected"],
                         borderColor: colours[2],
                         pointHighlightFill: colours[2],
                         fill: false
                     },
                     {
                         label: "R",
-                        data: [13,4,37,42,10,29,21],
+                        data: key["recovered"],
                         borderColor: colours[3],
                         pointHighlightFill: colours[3],
                         fill: false
                     },
                     {
                         label: "D",
-                        data: [16,28,33,21,2,44,23],                        
+                        data: key["deaths"],                        
                         borderColor: colours[4],
                         pointHighlightFill: colours[4],
                         fill: false
@@ -77,7 +93,8 @@ function sideCells(){
                 ],
             },
             options: {
-                title: { display: true, text: key },
+                title: { display: true, text: key["name"] },
+                legend: { display: isfirst },
                 responsive: true,
                 maintainAspectRatio: true,
                 scales: {
@@ -89,8 +106,30 @@ function sideCells(){
                     xAxes: [{
                         position: 'bottom'
                     }]
+                },
+                onClick: function(event, array) {
+                    mainCell(i);
                 }
             }
         });
+        cellCharts.push(newchart);
     });
 }
+
+function getCells(){
+    $.getJSON("/testing.json", function(data){
+        cellcont = data['cells'];
+        sideCells(data['cells']);
+        mainCell(0);   
+    });
+}
+
+
+
+
+// Main Javascript calls
+$(document).ready(function(){
+    getCells();
+});
+
+
